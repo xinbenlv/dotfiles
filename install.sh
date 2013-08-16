8d50099406780778e7e0660e8e431b365465015e
Zainan (Victor) Zhou <zzn@google.com>
2013-08-16 11:17:28 -0400

    Update install sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo "Start install dotfile, make sure you have git, vim, screen, tmux, tmuxinator in your machine already"
+
+echo "Git clone from Github"
+git clone https://github.com/xinbenlv/dotfiles.git ~/dotfiles
+echo "Set up submodules"
+cd ~/dotfiles
+git submodule init
+git submodule update
+git submodule sync
+
+echo "Backup old files to ~/dotfils.bak"
+
+mkdir ~/dotfiles.bak
+
+echo "Set up bashrc"
+mv ~/.bashrc ~/dotfiles.bak
+ln -s ~/dotfiles/bashrc ~/.bashrc
+
+echo "Set up vim"
+mv ~/.vimrc ~/dotfiles.bak
+mv ~/.vim ~/dotfiles.bak
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
+
+echo "Set up screenrc"
+mv ~/.screenrc ~/dotfiles.bak
+ln -s ~/dotfiles/screenrc ~/.screenrc
+
+echo "Set up tmux"
+mv ~/.tmux.conf ~/dotfiles.bak
+mv ~/.tmuxinator ~/dotfiles.bak
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmuxinator ~/.tmuxinator
+
+echo "Set up gitconfig"
+mv ~/.gitconfig ~/.dotfiles.bak
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+
+echo "Finished."
+echo "You will need to install powerline-font yourself, we recommend DejaVuSansMono"
+echo "https://github.com/Lokaltog/powerline-fonts/tree/master/DejaVuSansMono"
+
+
+
+
