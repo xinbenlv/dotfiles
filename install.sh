867c6fd1c42231c1508993e45c6341349e5508b8
Zainan Victor Zhou <zzn@zzn-macbookpro.roam.corp.google.com>
2017-12-21 01:10:05 -0800

    Delete a lot

--- install.sh
+++ install.sh
@@ -18,50 +18,27 @@ mv ~/.bashrc ~/dotfiles.bak
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/bashrc ~/.bash_profile
 
-
 echo "Set up vim"
 mv ~/.vimrc ~/dotfiles.bak
 mv ~/.vim ~/dotfiles.bak
-mv ~/.vimrc_vundle ~/dotfiles.bak
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vimrc_vundle ~/.vimrc_vundle
-echo "Install vim plugins"
-vim +PluginInstall +qall
-
-echo "Set up screenrc"
-mv ~/.screenrc ~/dotfiles.bak
-ln -s ~/dotfiles/screenrc ~/.screenrc
 
 echo "Set up tmux"
 mv ~/.tmux.conf ~/dotfiles.bak
 mv ~/.tmuxinator ~/dotfiles.bak
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/tmuxinator ~/.tmuxinator
-mv ~/.tmux-powerlinerc ~/dotfiles.bak
-ln -s ~/dotfiles/tmux-powerlinerc ~/.tmux-powerlinerc
-mv ~/.tmux ~/dotfiles.bak
-ln -s ~/dotfiles/tmux ~/.tmux
 
 # install tmux and tmuxinator 
 # TODO(zzn): find a way to handle sudo
 sudo apt-get install tmux
 sudo gem install tmuxinator
 
-echo "Set up emacs"
-mv ~/.emacs ~/dotfiles.bak
-ln -s ~/dotfiles/emacs ~/.emacs
-
-mv ~/.emacs.d ~/dotfiles.bak
-ln -s ~/dotfiles/emacs.d ~/.emacs.d
-
-
 echo "Set up git"
 mv ~/.gitconfig ~/.dotfiles.bak
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 mv ~/.gitignore ~/.gitignore.bak
 ln -s ~/dotfiles/gitignore ~/.gitignore
-
 
 echo "Finished."
 echo "You will need to install powerline-font yourself, we recommend DejaVuSansMono"
